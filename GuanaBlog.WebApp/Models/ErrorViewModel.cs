777f38f1dc3ebb9442478c9fec6085708fc4a7ae
juan_ <juan_@DESKTOP-I5MAECP>
2023-09-14 10:05:10 -0600

    Agregar archivos de proyecto.

---
+++ GuanaBlog.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GuanaBlog.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
